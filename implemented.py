62c1cec8a6e93d83c639de8653fc559adb08b46e
linkolnakakvant <karpovich.valera@mail.ru>
2022-11-20 13:34:27 +0300

    hard version

---
+++ implemented.py
@@ -0,0 +1,17 @@
+from dao.director import DirectorDAO
+from dao.genre import GenreDAO
+from dao.movie import MovieDAO
+
+from service.movie import MovieService
+from service.genre import GenreService
+from service.director import DirectorService
+
+from setup_db import db
+
+director_dao = DirectorDAO(session=db.session)
+genre_dao = GenreDAO(session=director_dao.session)
+movie_dao = MovieDAO(sission=db.session)
+
+director_service = DirectorService(dao=director_dao)
+genre_service = GenreService(dao=genre_dao)
+movie_service = MovieService(dao=movie_dao)
